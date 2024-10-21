f353f45ecf0d00b8fa38b3d391835d81d2c4cd73
Matt Radbourne <1254508+mradbourne@users.noreply.github.com>
2024-10-21 22:37:49 +0100

    Update keymaps

--- init.lua
+++ init.lua
@@ -302,11 +302,11 @@ require('lazy').setup({
       local builtin = require 'telescope.builtin'
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-      vim.keymap.set('n', '<leader>sf', function()
+      vim.keymap.set('n', '<leader>p', function()
         builtin.git_files {
           show_untracked = true,
         }
-      end, { desc = '[S]earch Git [F]iles' })
+      end, { desc = 'Search Git Files' })
       vim.keymap.set('n', '<leader>sc', function()
         builtin.git_status {
           initial_mode = 'normal',
@@ -314,12 +314,12 @@ require('lazy').setup({
       end, { desc = '[S]earch [C]hanged Files' })
       vim.keymap.set('n', '<leader>sa', builtin.find_files, { desc = '[S]earch [A]ll Files' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-      vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+      vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch Current [W]ord' })
+      vim.keymap.set('n', '<leader>f', builtin.live_grep, { desc = 'Search by Grep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-      vim.keymap.set('n', '<leader><leader>', function()
+      vim.keymap.set('n', '<Tab>', function()
         builtin.buffers {
           initial_mode = 'normal',
           attach_mappings = function(prompt_bufnr, map)
